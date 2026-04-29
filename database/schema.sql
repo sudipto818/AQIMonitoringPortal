dcab30284169f5edb1862e59e59bbb8302b4548b
Sujal-Patnaik <sujalpatnaik5@gmail.com>
2026-04-29 21:15:22 +0530

    Updated schema

--- database/schema.sql
+++ database/schema.sql
@@ -9,14 +9,17 @@ CREATE TABLE cities (
 );
 
 CREATE TABLE aqi_data (
-    id INT AUTO_INCREMENT PRIMARY KEY,   -- temporary PK
     city_id INT,
-    city_name VARCHAR(50),                    -- temporary (for CSV load)
     date DATE,
     co FLOAT,
     no2 FLOAT,
     o3 FLOAT,
     pm10 FLOAT,
     pm25 FLOAT,
-    aqi_daily FLOAT  
+    aqi_daily FLOAT,
+    PRIMARY KEY (city_id, date),
+    CONSTRAINT fk_city FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
+
+CREATE INDEX idx_date ON aqi_data(date);
+CREATE INDEX idx_city_aqi ON aqi_data(city_id, aqi_daily);
